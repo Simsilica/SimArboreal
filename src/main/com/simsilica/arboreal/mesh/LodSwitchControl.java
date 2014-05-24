67eae1482f8bd49485de4fcad1a6968ef0838ffa
pspeed42 <pspeed42@83352181-e685-4040-b55f-d83f507750f9>
2014-05-24 08:23:14 +0000

    The lod control now saves/loads properly so that the tree j3o can be saved with control intact.

--- src/main/com/simsilica/arboreal/mesh/LodSwitchControl.java
+++ src/main/com/simsilica/arboreal/mesh/LodSwitchControl.java
@@ -36,6 +36,11 @@
 
 package com.simsilica.arboreal.mesh;
 
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
+import com.jme3.export.Savable;
 import com.jme3.renderer.Camera;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
@@ -43,6 +48,7 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.Spatial.CullHint;
 import com.jme3.scene.control.AbstractControl;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -53,7 +59,8 @@ import java.util.List;
  *
  *  @author    Paul Speed
  */
-public class LodSwitchControl extends AbstractControl {
+public class LodSwitchControl extends AbstractControl
+                              implements Cloneable {
 
     private LodRange current = null;
     private List<LodRange> ranges = new ArrayList<LodRange>();
@@ -63,15 +70,33 @@ public class LodSwitchControl extends AbstractControl {
     public LodSwitchControl() {
     }
 
+    public LodSwitchControl clone() {
+        try {
+            LodSwitchControl c = (LodSwitchControl)super.clone();
+            
+            c.ranges = new ArrayList<LodRange>();
+            for( LodRange r : ranges ) {
+                c.ranges.add(r.clone());
+            }           
+            
+            return c;
+        } catch(CloneNotSupportedException e) {
+            throw new RuntimeException( "Can't clone control", e );
+        } 
+    }
+
     public void addLevel( float range, Spatial child ) {
-        LodRange r = new LodRange(range, child);
+        child.setCullHint(CullHint.Always);
+        ((Node)getSpatial()).attachChild(child);
+
+        int childIndex = ((Node)getSpatial()).getChildIndex(child);
+
+        LodRange r = new LodRange(range, childIndex);
         int index = Collections.binarySearch(ranges, r);
         if( index < 0 ) {
             index = -(index + 1); 
         } 
         ranges.add(index, r);
-        child.setCullHint(CullHint.Always);
-        ((Node)getSpatial()).attachChild(child);
         
         // If there is a range already set after this one
         // then adjust its near range
@@ -94,16 +119,36 @@ public class LodSwitchControl extends AbstractControl {
     }
  
     public void removeLevel( Spatial child ) {
+        int childIndex = ((Node)getSpatial()).getChildIndex(child);
+ 
+        int removed = 0;
         for( Iterator<LodRange> it = ranges.iterator(); it.hasNext(); ) {
             LodRange r = it.next();
-            if( r.child == child ) {
+            if( r.child == childIndex ) {
                 it.remove();
                 child.removeFromParent();
-                if( current != null && current.child == child ) {
+                if( current != null && current.child == childIndex ) {
                     current = null;
+                }
+                removed++;
+            } 
+        }
+ 
+        if( removed > 0 ) {       
+            // Make sure the other ranges have their indexes adjusted
+            // to account for the miss
+            for( Iterator<LodRange> it = ranges.iterator(); it.hasNext(); ) {
+                LodRange r = it.next();
+                if( r.child > childIndex ) {
+                    // Need to subtract to account for the removal
+                    r.child -= removed;
                 }
             }
         }
+    }
+ 
+    protected Spatial getChild( int index ) {
+        return ((Node)getSpatial()).getChild(index);
     }
  
     protected final LodRange findLevel( float distSq ) {
@@ -121,12 +166,12 @@ public class LodSwitchControl extends AbstractControl {
             return;
         }
         if( current != null ) {
-            current.child.setCullHint(CullHint.Always); 
+            getChild(current.child).setCullHint(CullHint.Always); 
         }
         current = findLevel(distSq);
         
         if( current != null ) {
-            current.child.setCullHint(CullHint.Inherit);
+            getChild(current.child).setCullHint(CullHint.Inherit);
         }       
     }
 
@@ -155,21 +200,73 @@ public class LodSwitchControl extends AbstractControl {
             camera = vp.getCamera();
         }
     }
+ 
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        super.write(ex);
+        OutputCapsule oc = ex.getCapsule(this);
+        oc.writeSavableArrayList((ArrayList)ranges, "ranges", new ArrayList());
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        super.read(im);
+        InputCapsule ic = im.getCapsule(this);
+        ranges = (ArrayList<LodRange>)ic.readSavableArrayList("ranges", new ArrayList());
+    }
+ 
+    @Override
+    public String toString() {
+        return "LodSwitchControl[" + ranges + "]";
+    }
     
-    protected class LodRange implements Comparable<LodRange> {
+    public static class LodRange implements Comparable<LodRange>, Savable {
         float nearSq;
         float farSq;
-        Spatial child;
+        int child;
         
-        public LodRange( float range, Spatial child ) {
+        public LodRange( float range, int child ) {
             this.nearSq = 0;
             this.farSq = range * range;
             this.child = child;
+        }
+        
+        public LodRange() {       
+        }
+
+        public LodRange clone() {
+            LodRange result = new LodRange();
+            result.nearSq = nearSq;
+            result.farSq = farSq;
+            result.child = child;
+            return result;
         }
 
         @Override
         public int compareTo( LodRange t ) {
             return (int)(farSq - t.farSq);
+        }
+
+        @Override
+        public void write( JmeExporter ex ) throws IOException {
+            OutputCapsule oc = ex.getCapsule(this);
+            oc.write(nearSq, "nearSq", 0);
+            oc.write(farSq, "farSq", 0);
+            oc.write(child, "child", -1);
+        }
+
+        @Override
+        public void read( JmeImporter im ) throws IOException {
+            InputCapsule ic = im.getCapsule(this);
+            this.nearSq = ic.readFloat("nearSq", 0);
+            this.farSq = ic.readFloat("farSq", 0);
+            //this.child = (Spatial)ic.readSavable("child", null);
+            this.child = ic.readInt("child", -1);
+        }
+        
+        @Override 
+        public String toString() {
+            return "LodRange[" + nearSq + ", " + farSq + ", " + child + "]";
         }
     }
 }
